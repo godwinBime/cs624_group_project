fbb438b0dd02ed11e7c165bc6f1987fc291e5afa
godwinBime <74328112+godwinBime@users.noreply.github.com>
2023-12-08 13:55:40 +0000

    Implemented the style for the overall application

---
+++ saveone/src/FrontEnd/style/style.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+
+const saveOneStyles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    itemDescripitionCard:{
+        marginTop: 10,
+        marginRight: 10,
+        marginLeft: 10,
+        marginBottom: 10,
+        borderStyle: "solid",
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: 'black'
+    },
+    itemImage:{
+        width: 100,
+        height: 120
+    },
+
+    itemButton:{
+        justifyContent: 'center'
+    },
+    itemText: {
+        marginTop: 10,
+        marginRight: 10,
+        marginLeft: 10,
+        marginBottom: 30,
+    }
+})
+
+export default saveOneStyles;
+
+
